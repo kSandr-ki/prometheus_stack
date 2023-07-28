964257ecfd7f470e64cfaa1032d797ddfa5added
Ksandr Ki <cyberside.reality@gmail.com>
2023-07-28 13:15:23 +0400

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run --rm -ti --entrypoint="/bin/sh" -v `pwd`:/prom prom/prometheus -c "promtool check config /prom/prometheus/prometheus.yml" && [[ $@ =~ "--reload" ]] && echo reloading && docker-compose exec prometheus kill -HUP 1
+#docker run --rm -ti --entrypoint="/bin/sh" -v `pwd`:/prom prom/prometheus -c "promtool check config /prom/prometheus/prometheus.yml" && [[ $@ =~ "--reload" ]] && echo reloading && docker-compose exec prometheus kill -HUP 1
+
+check_prom_config() {
+docker-compose exec prometheus promtool check config /etc/prometheus/prometheus.yml
+}
+
+reload_prom_config(){
+docker-compose exec prometheus kill -HUP 1
+}
+
+check_prom_config && [[ $@ =~ "--reload" ]] && echo reloading && reload_prom_config
